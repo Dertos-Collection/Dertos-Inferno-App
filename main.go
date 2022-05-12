fe06f9568a7d3b139eea6e7d410b70312b355d20
ZOrfeas <orfeas.zografos@gmail.com>
2022-05-12 21:53:37 +0300

    Add fyne hello world

--- main.go
+++ main.go
@@ -1,30 +1,15 @@
 package main
 
 import (
-	"log"
-	"os"
-
-	"gioui.org/app"
-	"gioui.org/font/gofont"
-	"gioui.org/unit"
-	"gioui.org/widget/material"
-	"github.com/Dertos-Collection/Dertos-Inferno-App/view"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
-	ui := view.UI{
-		W: app.NewWindow(
-			app.Title("Dertos' Inferno"),
-			app.Size(unit.Dp(9*40), unit.Dp(16*40)),
-		),
-		Th: material.NewTheme(gofont.Collection()),
-	}
-	go func() {
-		if err := ui.Run(); err != nil {
-			log.Println(err)
-			os.Exit(1)
-		}
-		os.Exit(0)
-	}()
-	app.Main()
+	a := app.New()
+	w := a.NewWindow("Dertos' Inferno")
+	w.Resize(fyne.NewSize(9*30, 16*30))
+	w.SetContent(widget.NewLabel("Είμαι ο Ντέρτος και θα σε φάω"))
+	w.ShowAndRun()
 }
