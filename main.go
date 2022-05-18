b509e373e809fd901f83a40050b373bd1f88a55b
ZOrfeas <orfeas.zografos@gmail.com>
2022-05-18 14:59:17 +0300

    Dummy commit

--- main.go
+++ main.go
@@ -3,13 +3,50 @@ package main
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
+type State int
+
+const (
+	Menu State = iota
+	Settings
+	Pause
+	Play
+)
+
+type ui struct {
+	state State
+	self  *fyne.Container
+}
+
+func (u *ui) changeState(state State) {
+	u.state = state
+	u.self.Refresh()
+}
+
+func (u *ui) load() fyne.CanvasObject {
+	// TODO: Implement
+	title := container.NewCenter(
+		widget.NewLabel("Dertos' Inferno"),
+	)
+
+	u.self = container.NewBorder(
+		title, nil, nil, nil,
+	)
+	return u.self
+}
+
 func main() {
 	a := app.New()
+	// a := app.NewWithID("com.dertosCollection.inferno")
+	// a.SetIcon()
+	// a.Settings().SetTheme()
 	w := a.NewWindow("Dertos' Inferno")
+
+	infernoUi := &ui{}
+	w.SetContent(infernoUi.load())
 	w.Resize(fyne.NewSize(9*30, 16*30))
-	w.SetContent(widget.NewLabel("Είμαι ο Ντέρτος και θα σε φάω"))
 	w.ShowAndRun()
 }
